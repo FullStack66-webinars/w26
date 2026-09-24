b20f92d292ce201e818d406cf27f0a61a56e0569
matveyromodanovskiy <startpolus@gmail.com>
2026-09-24 10:02:47 +0300

    refactor aiService and add config

--- hw27/config.js
+++ hw27/config.js
@@ -3,7 +3,7 @@ const ROLES = {
     ADMIN: 'ADMIN',
 };
 
-const AI_MODEL = "gemini-3-flash-preview";
+const AI_MODEL = "gemini-3.5-flash";
 
 const FRIDGE_FILE = "../fridge.json";
 export {ROLES, AI_MODEL, FRIDGE_FILE};
